da63d12349f1ce8bbbf47768632ba8a94bf18441
diii2409 <duynhut366@gmail.com>
2025-02-18 00:01:20 +0700

    fix bug in sidebar of shadcn  ( lack of DialogTitle )

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,18 +1,24 @@
 "use client";
 
-import {Slot} from "@radix-ui/react-slot";
-import {VariantProps, cva} from "class-variance-authority";
-import {PanelLeft} from "lucide-react";
-import * as React from "react";
-
 import {Button} from "@/components/ui/button";
 import {Input} from "@/components/ui/input";
 import {Separator} from "@/components/ui/separator";
-import {Sheet, SheetContent} from "@/components/ui/sheet";
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetHeader,
+	SheetTitle,
+} from "@/components/ui/sheet";
 import {Skeleton} from "@/components/ui/skeleton";
 import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
 import {useIsMobile} from "@/hooks/use-mobile";
 import {cn} from "@/lib/utils";
+import {Slot} from "@radix-ui/react-slot";
+import {VisuallyHidden} from "@radix-ui/react-visually-hidden";
+import {VariantProps, cva} from "class-variance-authority";
+import {PanelLeft} from "lucide-react";
+import * as React from "react";
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state";
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
@@ -186,6 +192,12 @@ const Sidebar = React.forwardRef<
 							} as React.CSSProperties
 						}
 						side={side}>
+						<VisuallyHidden>
+							<SheetHeader>
+								<SheetTitle>Sidebar</SheetTitle>
+								<SheetDescription>Sidebar</SheetDescription>
+							</SheetHeader>
+						</VisuallyHidden>
 						<div className="flex h-full w-full flex-col">{children}</div>
 					</SheetContent>
 				</Sheet>
