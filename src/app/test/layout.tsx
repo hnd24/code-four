a8ca7d1cf70a9e9c367adc99679f70428afe834e
diii2409 <duynhut366@gmail.com>
2025-02-27 04:20:49 +0700

    remove test page

--- src/app/test/layout.tsx
+++
@@ -1,6 +0,0 @@
-import {Providers} from "@/app/test/Provider";
-import "../globals.css";
-
-export default function Layout({children}: {children: React.ReactNode}) {
-	return <Providers>{children}</Providers>;
-}
